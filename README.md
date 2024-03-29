729ba0418f1ea7aed75d559d5f3557612d0bbe52
JBSL <bvkvgxvt@gmail.com>
2024-03-29 19:31:57 +0100

    First school model, with simple question/answer Removed .idea and .rasa from tracking

---
+++ README.md
@@ -0,0 +1,3 @@
+Python 3.9.13
+
+python -m spacy download es_core_news_md
--- actions/actions.py
+++ actions/actions.py
@@ -5,23 +5,31 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+class ActionInformClassSchedule(Action):
+
+    def name(self) -> Text:
+        return "action_inform_class_schedule"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        schedule_type = tracker.get_slot("schedule_type")
+
+        if schedule_type == "morning":
+            message = "Las clases en turno de mañana comienzan a las 8:15 a.m."
+        elif schedule_type == "afternoon":
+            message = "Las clases en turno de tarde comienzan a las 2:00 p.m."
+        else:
+            message = "Por favor, especifica si deseas conocer el horario de la mañana o de la tarde."
+
+        dispatcher.utter_message(text=message)
+
+        return []
