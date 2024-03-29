b6af1f415eb68faade9f89b6a089de9c4f9ea41a
JBSL <bvkvgxvt@gmail.com>
2024-03-29 21:26:02 +0100

    - Simple conversations done

--- actions/actions.py
+++ actions/actions.py
@@ -6,30 +6,21 @@
 
 
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+import csv
 
 
-class ActionInformClassSchedule(Action):
-
+class ActionProfessionalFamilies(Action):
     def name(self) -> Text:
-        return "action_inform_class_schedule"
+        return "action_professional_families"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        families_list = "Aquí están las familias profesionales disponibles:\n"
+        with open('external_data/Families.csv', mode='r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                families_list += f"- {row['Family']}\n"
 
-        schedule_type = tracker.get_slot("schedule_type")
-
-        if schedule_type == "morning":
-            message = "Las clases en turno de mañana comienzan a las 8:15 a.m."
-        elif schedule_type == "afternoon":
-            message = "Las clases en turno de tarde comienzan a las 2:00 p.m."
-        else:
-            message = "Por favor, especifica si deseas conocer el horario de la mañana o de la tarde."
-
-        dispatcher.utter_message(text=message)
-
+        dispatcher.utter_message(text=families_list)
         return []
